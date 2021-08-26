366085d156773b9e375390b6f69492b9e7db6ad8
k-kashapov <52855633+k-kashapov@users.noreply.github.com>
2021-08-26 17:15:38 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -13,13 +13,15 @@ int main (int argc, char* argv[])
 {
     int num_of_lines = 0;
 
+    printf ("Reading file...\n");
     char **strings = read_all_lines (&num_of_lines, "source.txt");
     
-    //show_res (strings, num_of_lines);
-    //return 0;
-
+    printf ("Sorting...\n");
     strings = merge_sort (strings, 0, num_of_lines);
+    
+    printf ("Printing output...\n");
     show_res(strings, num_of_lines);
 
+    printf ("Done!\n");
     return 0;
 }
